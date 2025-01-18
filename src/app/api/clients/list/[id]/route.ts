b6bd147613be8b36ad813b4740a5b5ab8de661b1
patrckak <patrickoliveira.silva20@gmail.com>
2025-01-18 17:39:36 -0300

    fix: fixed typed route

--- src/app/api/clients/list/[id]/route.ts
+++ src/app/api/clients/list/[id]/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: any } }
 ) {
   const id = params.id;
 
