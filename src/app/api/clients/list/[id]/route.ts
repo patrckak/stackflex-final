260ce2533753f0ac126805a303e2500abca03e52
patrckak <patrickoliveira.silva20@gmail.com>
2025-01-18 17:45:36 -0300

    fix

--- src/app/api/clients/list/[id]/route.ts
+++ src/app/api/clients/list/[id]/route.ts
@@ -3,20 +3,28 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: any } }
+  { params }: { params: { id: string } } // Alterado para string para garantir o tipo correto
 ) {
-  const id = params.id;
+  const { id } = params;
 
   if (id) {
-    let clients = await prisma.client.findMany({
-      where: { id: id },
-      select: { clientId: true, name: true },
-    });
+    try {
+      let clients = await prisma.client.findMany({
+        where: { id: id },
+        select: { clientId: true, name: true },
+      });
 
-    if (clients.length > 0) {
-      return NextResponse.json({ data: clients });
-    } else {
-      return NextResponse.json({ data: [] });
+      if (clients.length > 0) {
+        return NextResponse.json({ data: clients });
+      } else {
+        return NextResponse.json({ data: [] });
+      }
+    } catch (error) {
+      // Tratamento de erro
+      return NextResponse.json(
+        { error: "Erro ao buscar clientes." },
+        { status: 500 }
+      );
     }
   } else {
     return NextResponse.json({ data: [] });
