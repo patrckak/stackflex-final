30bcc18a86f473cc87066d66c42a1b905533ea84
patrckak <patrickoliveira.silva20@gmail.com>
2025-01-18 12:49:01 -0300

    fix: get clients route fix

--- src/app/api/clients/list/[id]/route.ts
+++ src/app/api/clients/list/[id]/route.ts
@@ -1,20 +1,23 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
-  const id = (await params).id;
+  const id = params.id;
 
   if (id) {
     let clients = await prisma.client.findMany({
       where: { id: id },
       select: { clientId: true, name: true },
     });
-    if (clients) {
+
+    if (clients.length > 0) {
       return NextResponse.json({ data: clients });
-    } else return [];
+    } else {
+      return NextResponse.json({ data: [] }); 
   } else {
-    return NextResponse.json({});
+    return NextResponse.json({ data: [] });
   }
 }
