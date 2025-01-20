8e13423d05cb5a882e1c646f85690935f7f9ea6a
patrckak <patrickoliveira.silva20@gmail.com>
2025-01-20 13:21:47 -0300

    fix: tsx file

--- src/app/(session)/app/servicos/novo/cliente/page.tsx
+++ src/app/(session)/app/servicos/novo/cliente/page.tsx
@@ -23,7 +23,7 @@ import { clientForm } from "../../../../../../../utils/schemas";
 import { newClient } from "@/app/actions/client";
 import { useToast } from "@/hooks/use-toast";
 
-export default function GerarCliente() {
+export default function Page() {
   const { data: session } = useSession();
 
   const form = useForm<z.infer<typeof clientForm>>({
