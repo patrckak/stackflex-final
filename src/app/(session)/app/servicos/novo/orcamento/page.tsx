29e5d6a0e7385a5ac60c9eee87081f383b735599
patrckak <patrickoliveira.silva20@gmail.com>
2025-01-22 20:57:27 -0300

    fix: import conflict

--- src/app/(session)/app/servicos/novo/orcamento/page.tsx
+++ src/app/(session)/app/servicos/novo/orcamento/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AppSidebar } from "@/components/app-sidebar";
-import EstimateForm from "@/components/ui/estimate-form";
 import ThemedSection from "@/components/ui/themedSection";
 import Layout from "./layout";
 import { useSession } from "next-auth/react";
