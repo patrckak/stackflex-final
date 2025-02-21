a3b3f5bf6eec6ef22afeb0708e505a400c061d87
patrckak <patrickoliveira.silva20@gmail.com>
2025-02-21 20:55:12 -0300

    corrigir: atualizar página de senha para melhorar a segurança
    - Modificada a página de senha para aprimorar as medidas de segurança.

--- src/app/v/[id]/[[...password]]/page.tsx
+++ src/app/v/[id]/[[...password]]/page.tsx
@@ -7,32 +7,46 @@ import {
 } from "@/components/ui/input-otp";
 import ThemedSection from "@/components/ui/themedSection";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const params = useParams<{ id: string; password: string }>();
 
-  const [password, setPassword] = useState<string>();
+  const [password, setPassword] = useState<string>("");
+  const [passwordStatus, setPasswordStatus] = useState<boolean>(false);
+
+  //? verificar se a senha foi passada por parametro
+  useEffect(() => {
+    if (params.password != undefined) {
+      setPasswordStatus(true);
+      setPassword(params.password);
+    }
+  }, [password]);
+
+  //? verificar se a senha foi digitada
+  useEffect(() => {
+    if (password.length == 6) {
+      setPasswordStatus(true);
+      //? recuperar dados do orcamento
+    }
+  }, [password]);
 
   const handlePasswordInput = (e) => {
     setPassword(e);
   };
 
-  if (password.length == 6) {
-    //? recuperar dados do orcamento
-  }
-
-  if (params.password != undefined) {
+  if (passwordStatus) {
     return (
       <ThemedSection>
         <div className="flex flex-col items-center justify-center h-screen">
-          {params.password} - {params.id}
+          {password} - {params.id}
         </div>
       </ThemedSection>
     );
   }
   return (
     <ThemedSection>
+      senha: {password}
       <div className="flex flex-col items-center justify-center h-screen">
         <span className="p-5 rounded-md bg-zinc-300 dark:bg-zinc-200 flex flex-col gap-3 items-center">
           <h3 className="">Senha de acesso ao orçamento.</h3>
