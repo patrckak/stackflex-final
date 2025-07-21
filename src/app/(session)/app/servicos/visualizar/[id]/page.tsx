e21632d1b3d6363a0030fc8165584c4f5d7ad54d
Patrick Oliveira <patrickoliveira.silva20@gmail.com>
2025-07-20 22:18:10 -0300

    feat: substitui indicador de carregamento por mensagem de texto e componente ReactLoading

--- src/app/(session)/app/servicos/visualizar/[id]/page.tsx
+++ src/app/(session)/app/servicos/visualizar/[id]/page.tsx
@@ -101,14 +101,8 @@ export default function VisualizarOrcamento() {
               <PreviewEstimate data={data} />
             ) : (
               <span className="flex h-[100%] justify-center items-center">
-                <l-ring-2
-                  size="40"
-                  stroke="5"
-                  stroke-length="0.25"
-                  bg-opacity="0.1"
-                  speed="0.8"
-                  color="black"
-                ></l-ring-2>
+                Carregando dados do orçamento...
+                <ReactLoading />
               </span>
             )}
           </div>
